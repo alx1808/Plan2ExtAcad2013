d32404761c8f7b958092c590ea7262e9ed37bcab
Alexander Ausweger <alexander.ausweger@aon.at>
2019-03-15 08:32:21 +0100

    Plan2LayerKontrolle

---
+++ Plan2Ext/LayerKontrolle/Commands.cs
@@ -0,0 +1,115 @@
+﻿using System.Globalization;
+using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.EditorInput;
+using Autodesk.AutoCAD.Runtime;
+using Application = Autodesk.AutoCAD.ApplicationServices.Core.Application;
+// ReSharper disable StringLiteralTypo
+// ReSharper disable IdentifierTypo
+
+namespace Plan2Ext.LayerKontrolle
+{
+    // ReSharper disable once UnusedMember.Global
+    public class Commands
+    {
+        //#region log4net Initialization
+        //private static readonly log4net.ILog Log = log4net.LogManager.GetLogger(System.Convert.ToString((typeof(Commands))));
+        //#endregion
+
+        static Palette _Palette;
+
+        [CommandMethod("Plan2LayerKontrolle")]
+        // ReSharper disable once UnusedMember.Global
+        public void Plan2LayerKontrolle()
+        {
+            HandleSetLayers(true);
+        }
+        [CommandMethod("Plan2LayerKontrolleSetLayers")]
+        // ReSharper disable once UnusedMember.Global
+        public void Plan2LayerKontrolleSetLayers()
+        {
+            HandleSetLayers(false);
+        }
+
+        private void HandleSetLayers(bool initLayers)
+        {
+            try
+            {
+                OpenPalette();
+                if (initLayers) _Palette.InitLayers();
+
+                var doc = Application.DocumentManager.MdiActiveDocument;
+                if (doc == null) return;
+
+                // ReSharper disable once UnusedVariable
+                using (var mDoclock = doc.LockDocument())
+                {
+
+#if NEWSETFOCUS
+                    doc.Window.Focus();
+#else
+                Autodesk.AutoCAD.Internal.Utils.SetFocusToDwgView(); // previous 2014 AutoCAD - Versions
+#endif
+
+                    _Palette.SetLayers();
+                }
+            }
+            catch (System.Exception ex)
+            {
+                Application.ShowAlertDialog(string.Format(CultureInfo.CurrentCulture,
+                    "Fehler in Plan2LayerKontrolle aufgetreten! {0}", ex.Message));
+            }
+        }
+
+        [CommandMethod("Plan2LayerKontrolleSelectAlwaysOnLayer")]
+        // ReSharper disable once UnusedMember.Global
+        public static void Plan2LayerKontrolleSelectAlwaysOnLayer()
+        {
+            try
+            {
+                OpenPalette();
+
+                Document doc = Application.DocumentManager.MdiActiveDocument;
+
+                // ReSharper disable once UnusedVariable
+                using (var mDoclock = doc.LockDocument())
+                {
+                    var ed = doc.Editor;
+#if NEWSETFOCUS
+                    doc.Window.Focus();
+#else
+                    Autodesk.AutoCAD.Internal.Utils.SetFocusToDwgView(); // previous 2014 AutoCAD - Versions
+#endif
+
+                    var per = ed.GetEntity("\nElement wählen, dessen Layer immer angezeigt werden soll: ");
+                    if (per.Status == PromptStatus.OK)
+                    {
+                        using (var tr = doc.TransactionManager.StartTransaction())
+                        {
+                            Entity entity = tr.GetObject(per.ObjectId, OpenMode.ForRead) as Entity;
+                            if (entity == null) return;
+                            _Palette.AddAlwaysOnLayer(entity.Layer);
+                            tr.Commit();
+                        }
+                    }
+                }
+            }
+            catch (System.Exception ex)
+            {
+                Application.ShowAlertDialog(string.Format(CultureInfo.CurrentCulture, "Fehler in Plan2LayerKontrolleSelectAlwaysOnLayer aufgetreten! {0}", ex.Message));
+            }
+        }
+
+
+        private static void OpenPalette()
+        {
+
+            if (_Palette == null)
+            {
+                _Palette = new Palette();
+            }
+
+            _Palette.Show();
+        }
+    }
+}
---
+++ Plan2Ext/LayerKontrolle/Palette.cs
@@ -0,0 +1,101 @@
+﻿using Autodesk.AutoCAD.ApplicationServices.Core;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.Windows;
+// ReSharper disable IdentifierTypo
+// ReSharper disable StringLiteralTypo
+
+namespace Plan2Ext.LayerKontrolle
+{
+    public class Palette
+    {
+        private static PaletteSet _PaletteSet;
+        private static LayerKontrolleControl _Control;
+
+        public Palette()
+        {
+            _Control = new LayerKontrolleControl();
+        }
+
+        private string CurrentLayerName
+        {
+            get { return _Control.CurrentLayerName; }
+        }
+
+        public void Show()
+        {
+            if (_PaletteSet == null)
+            {
+                _PaletteSet = new PaletteSet("LayerKontrolle")
+                {
+                    Style = PaletteSetStyles.NameEditable |
+                            PaletteSetStyles.ShowPropertiesMenu |
+                            PaletteSetStyles.ShowAutoHideButton |
+                            PaletteSetStyles.ShowCloseButton,
+                    MinimumSize = new System.Drawing.Size(170, 164)
+                };
+#if ACAD2013_OR_NEWER
+                _PaletteSet.SetSize(new System.Drawing.Size(210, 164));
+#endif
+                _PaletteSet.Add("LayerKontrolle", _Control);
+
+                if (!_PaletteSet.Visible)
+                {
+                    _PaletteSet.Visible = true;
+                }
+
+            }
+            else
+            {
+                if (!_PaletteSet.Visible)
+                {
+                    _PaletteSet.Visible = true;
+                }
+            }
+
+        }
+
+        internal void InitLayers()
+        {
+            _Control.InitLayers();
+        }
+
+        internal void SetLayers()
+        {
+
+            var doc = Application.DocumentManager.MdiActiveDocument;
+            var db = doc.Database;
+            using (var trans = doc.TransactionManager.StartTransaction())
+            {
+                var layTb = (LayerTable)trans.GetObject(db.LayerTableId, OpenMode.ForRead);
+                foreach (var ltrOid in layTb)
+                {
+                    bool dontFreeze = (ltrOid == db.Clayer);
+                    var ltr = (LayerTableRecord)trans.GetObject(ltrOid, OpenMode.ForRead);
+                    var name = ltr.Name;
+                    var on = IsAlwaysOn(name) || (name == CurrentLayerName);
+                    SetLayer(ltr, !on, dontFreeze);
+                }
+                trans.Commit();
+            }
+        }
+
+        private void SetLayer(LayerTableRecord ltr, bool off, bool dontFreeze)
+        {
+            if (!ltr.IsFrozen && ltr.IsOff == off) return;
+
+            ltr.UpgradeOpen();
+            if (!dontFreeze) ltr.IsFrozen = false;
+            ltr.IsOff = off;
+        }
+
+        public void AddAlwaysOnLayer(string entityLayer)
+        {
+            _Control.AddAlwaysOnLayer(entityLayer);
+        }
+
+        private bool IsAlwaysOn(string name)
+        {
+            return _Control.IsAlwaysOn(name);
+        }
+    }
+}
